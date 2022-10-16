18644b1647d8e3d502e9683d4f144d13c6c25ea6
ragul-engg <ragulshanmugarajan21@gmail.com>
2022-10-16 17:12:54 +0530

    cleared dotenv

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const datas = require("./apis/routes/datas")
 const searchData = require("./apis/routes/searchData")
 const mongoose = require('mongoose')
-const env = require('dotenv').config()
 const app = express()
+
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 let port = process.env.PORT || 3000
 
